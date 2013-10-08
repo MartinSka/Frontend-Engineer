6c96a3a82b80b88ae4db5bf26a91551074d32f5f
Martin <mxpx1039@hotmail.com>
2013-10-07 23:07:23 -0300

    Css + InfoTaks

--- public/js/main.js
+++ public/js/main.js
@@ -13,18 +13,21 @@ require(['tasks/task-router', 'tasks/task-view', 'dpd'], function(Router, View, 
 
     $('#new-task').submit(function(e) {
     	e.preventDefault();
-    	value = $(this).find('input')
+    	value = $(this).find('input');
     	
     	task = new Task({title: value.val()});
-    	viewtasksList.addOne(task);
 
     	task.save({
     		success: function(){
+                console.log('sucess');
+                viewtasksList.addOne(task);
     			value.val('');
     		}
     	});
-
-    	value.val('');    	
+        
+        viewtasksList.addOne(task);
+        value.val('');
+	
     });    
 
 });
--- public/js/tasks/task-view.js
+++ public/js/tasks/task-view.js
@@ -4,44 +4,98 @@ define(
 	],
 
 	function($, _, Backbone, TasksList){
+
+		var TaskInfoView = Backbone.View.extend({
+			el: '#sidebar',
+
+			template: _.template( $('#taskInfo-template').html() ),
+
+			initialize: function(){
+				this.render();
+				this.listenTo(this.model, 'change', this.render);
+			},
+
+			render: function(){
+				console.log('render');
+				this.$el.html('');
+				var template = this.template( this.model.toJSON() );
+				this.$el.html( template );
+				return this;
+			}
+		});
 		
 		var TaskView = Backbone.View.extend({
 			tagName: 'li',
+			className: 'panel',
+
+			events:{
+				'click [data-icon="trash"]': 'delete',
+				'blur [contenteditable]': 'update',
+				'keypress [contenteditable]': 'updateEnter',
+				'click': 'taskInfo'
+			},
 
 			template: _.template( $('#task-template').html() ),
-
-			initialize: function(value){
-				task = new Task({title: value});
-				t = task.render();
-				$('#bla').append(t);
-			},
 
 			render: function(){
 				var template = this.template( this.model.toJSON() );
 				this.$el.html(template);
 				return this;
+			},
+
+			delete: function(){
+				that = this;
+				this.model.destroy({
+					success: function(){
+						console.log('success');
+						that.remove();
+					}
+				});
+				this.remove();
+			},
+
+			update: function(){				
+				title = this.model.toJSON();
+				newTitle = this.$el.find('label').html();
+				
+				if(title.title != newTitle){
+					this.model.set( { title: newTitle } );
+					this.model.save();
+				};
+			},
+
+			updateEnter: function(e){
+				if (e.keyCode === 13){
+					this.$el.find('label').blur();
+					this.update();
+				};
+
+				//Ver tema del keypress, keyup, keydown
+				newTitle = this.$el.find('label').html();
+				$('#sidebar').find('h3').html(newTitle);
+			},
+
+			taskInfo: function(){
+				var taskInfo = new TaskInfoView({model: this.model});
 			}
 		});				
 
 		var TasksListView = Backbone.View.extend({
-			el: $('#bla'),
+			el: $('#tasks'),			
 
 			render: function(){
 				var that = this;
 
 				tasksList.fetch({
 					success: function(){
-						//that.$el.html('');
-						tasksList.each(function(task) {
-							 that.addOne(task);
+						tasksList.each(function(task) {							
+							that.addOne(task);
 						});
 					}
 				});
 			},
 
 			addOne: function(task){
-				console.log(task);
-			
 				taskView = new TaskView({model: task});
 				this.$el.append( taskView.render().el );
 			},
